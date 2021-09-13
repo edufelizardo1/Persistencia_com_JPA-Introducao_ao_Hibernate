058c4c1a56ed566d1859945628ecf8f8559ea34c
Cândido <edufelizardo1@gamil.com>
2021-09-13 18:27:12 -0300

    Update CadastroDeProduto.java

--- src/main/java/loja/testes/CadastroDeProduto.java
+++ src/main/java/loja/testes/CadastroDeProduto.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 
 import javax.persistence.EntityManager;
 
+import loja.dao.CategoriaDao;
 import loja.dao.ProdutoDao;
 import loja.modelo.Categoria;
 import loja.modelo.Produto;
@@ -14,11 +15,15 @@ public class CadastroDeProduto {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Produto celular = new Produto("Xiomi Readme", "Muito legal", new BigDecimal("800"), Categoria.CELULARES);
+		Categoria celulares = new Categoria("CELULARES");
+		Produto celular = new Produto("Xiomi Readme", "Muito legal", new BigDecimal("800"), celulares );
 		EntityManager entityManager = JPAUtil.getEntityManager();
 		ProdutoDao produtoDao = new ProdutoDao(entityManager);
+		CategoriaDao categoriaDao = new CategoriaDao(entityManager);
 		
 		entityManager.getTransaction().begin();
+		
+		categoriaDao.cadastrar(celulares);
 		produtoDao.cadastrar(celular);
 		entityManager.getTransaction().commit();
 		entityManager.close();
