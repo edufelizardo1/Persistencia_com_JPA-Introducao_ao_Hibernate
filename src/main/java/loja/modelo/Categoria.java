c4c2feafbb9f701d20ff10eb7b2daac53a8b74da
Cândido <edufelizardo1@gamil.com>
2021-09-13 18:27:04 -0300

    Update Categoria.java

--- src/main/java/loja/modelo/Categoria.java
+++ src/main/java/loja/modelo/Categoria.java
@@ -1,9 +1,36 @@
 package loja.modelo;
 
-public enum Categoria {
-	
-	CELULARES,
-	INOFRMATICA,
-	LIVROS;
+import javax.persistence.*;
 
+@Entity
+@Table(name = "categorias")
+public class Categoria {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nome;
+	
+	public Categoria(String nome) {
+		super();
+		this.nome = nome;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
 }
